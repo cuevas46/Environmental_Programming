d40b6a6037fecbfdf6249b5e295e23f8490416d5
cuevas46 <jm.cuevas.davila@gmail.com>
2023-01-23 09:21:46 +0100

    Update Alis code to my PC

--- Landslide.py
+++ Landslide.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 from osgeo import gdal
 import sys
 import os
-sys.path.append('/Users/alismac/Documents/Environmental_Programming/Project/A3_Landslide_detection')
+sys.path.append('/Users/cuevas46/Documents/Environmental_Programming/Project/A3_Landslide_detection')
 import GIS_functions as gf
 import datetime
 
-CDataDir = '/Users/alismac/Documents/Environmental_Programming/Project/A3_Landslide_detection/Data/Clipped_images'
-FDataDir = '/Users/alismac/Documents/Environmental_Programming/Project/A3_Landslide_detection/Data/Full_image'
+CDataDir = '/Users/cuevas46/Documents/Environmental_Programming/Project/A3_Landslide_detection/Data/Clipped_images'
+FDataDir = '/Users/cuevas46/Documents/Environmental_Programming/Project/A3_Landslide_detection/Data/Full_image'
 
 # folder path
 dir_path = CDataDir
@@ -24,79 +24,79 @@ for path in os.listdir(dir_path):
         res.append(path)
 
 print(res)
-
-BI = list()
-NDMI = list()
-NDVI = list()
-BI_Date = list()
-NDMI_Date =list()
-NDVI_Date = list()
-for i in range (0, len(res)):
-    temp_list = res[i].split('_')
-    if temp_list[1] == 'BI':
-        BI.append(res[i])
-        BI_Date.append((temp_list[2].split("."))[0])
-    elif temp_list[1] == 'NDMI':
-        NDMI.append(res[i])
-        NDMI_Date.append(temp_list[2].split(".")[0])
-    elif temp_list[1] == 'NDVI':
-        NDVI.append(res[i])
-        NDVI_Date.append(temp_list[2].split(".")[0])
-print(NDMI_Date)
-print(BI_Date)
-print(NDVI_Date)
-
-year =
-month
-
-avg_bi = list()
-for i in range (0,len(BI)):
-    ds = gdal.Open(CDataDir+'/'+BI[i])
-    band = ds.GetRasterBand(1)
-    no_data = band.GetNoDataValue()
-    array = band.ReadAsArray()
-    mean = np.mean(array[array > no_data])
-    avg_bi.append(mean)
-
-print(avg_bi)
-
-avg_ndmi = list()
-for i in range (0,len(NDMI)):
-    ds1 = gdal.Open(CDataDir+'/'+NDMI[i])
-    band1 = ds1.GetRasterBand(1)
-    no_data1 = band1.GetNoDataValue()
-    array1 = band1.ReadAsArray()
-    mean1 = np.mean(array1[array1 > no_data])
-    avg_ndmi.append(mean1)
-
-print(avg_ndmi)
-
-avg_ndvi = list()
-for i in range (0,len(NDVI)):
-    ds = gdal.Open(CDataDir+'/'+NDVI[i])
-    band = ds.GetRasterBand(1)
-    no_data = band.GetNoDataValue()
-    array = band.ReadAsArray()
-    mean = np.mean(array[array > no_data])
-    avg_ndvi.append(mean)
-
-print(avg_ndvi)
-
-dates = BI_Date + NDMI_Date + NDVI_Date
-dates
-Day =list()
-Month =list()
-Year =list()
-for i in range (0, (len(dates)-1)):
-    Year.append(dates[i][0:4])
-    Month.append(dates[i][4:6])
-    Day.append(dates[i][6:8])
-
-date_form = list()
-for i in range (0,len(dates)-1):
-    date = str(datetime.date(int(Year[i]),int(Month[i]),int(Day[i])))
-    date_form.append(date)
-
-date_form
-
-pd.dataframe
+#
+# BI = list()
+# NDMI = list()
+# NDVI = list()
+# BI_Date = list()
+# NDMI_Date =list()
+# NDVI_Date = list()
+# for i in range (0, len(res)):
+#     temp_list = res[i].split('_')
+#     if temp_list[1] == 'BI':
+#         BI.append(res[i])
+#         BI_Date.append((temp_list[2].split("."))[0])
+#     elif temp_list[1] == 'NDMI':
+#         NDMI.append(res[i])
+#         NDMI_Date.append(temp_list[2].split(".")[0])
+#     elif temp_list[1] == 'NDVI':
+#         NDVI.append(res[i])
+#         NDVI_Date.append(temp_list[2].split(".")[0])
+# print(NDMI_Date)
+# print(BI_Date)
+# print(NDVI_Date)
+#
+# year =
+# month
+#
+# avg_bi = list()
+# for i in range (0,len(BI)):
+#     ds = gdal.Open(CDataDir+'/'+BI[i])
+#     band = ds.GetRasterBand(1)
+#     no_data = band.GetNoDataValue()
+#     array = band.ReadAsArray()
+#     mean = np.mean(array[array > no_data])
+#     avg_bi.append(mean)
+#
+# print(avg_bi)
+#
+# avg_ndmi = list()
+# for i in range (0,len(NDMI)):
+#     ds1 = gdal.Open(CDataDir+'/'+NDMI[i])
+#     band1 = ds1.GetRasterBand(1)
+#     no_data1 = band1.GetNoDataValue()
+#     array1 = band1.ReadAsArray()
+#     mean1 = np.mean(array1[array1 > no_data])
+#     avg_ndmi.append(mean1)
+#
+# print(avg_ndmi)
+#
+# avg_ndvi = list()
+# for i in range (0,len(NDVI)):
+#     ds = gdal.Open(CDataDir+'/'+NDVI[i])
+#     band = ds.GetRasterBand(1)
+#     no_data = band.GetNoDataValue()
+#     array = band.ReadAsArray()
+#     mean = np.mean(array[array > no_data])
+#     avg_ndvi.append(mean)
+#
+# print(avg_ndvi)
+#
+# dates = BI_Date + NDMI_Date + NDVI_Date
+# dates
+# Day =list()
+# Month =list()
+# Year =list()
+# for i in range (0, (len(dates)-1)):
+#     Year.append(dates[i][0:4])
+#     Month.append(dates[i][4:6])
+#     Day.append(dates[i][6:8])
+#
+# date_form = list()
+# for i in range (0,len(dates)-1):
+#     date = str(datetime.date(int(Year[i]),int(Month[i]),int(Day[i])))
+#     date_form.append(date)
+#
+# date_form
+#
+# pd.dataframe
